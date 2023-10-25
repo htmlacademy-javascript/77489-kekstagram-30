627b31436d28d5f3e79b69da64dfd2a2c1df6e11
Дмитрий Сорочкин <ancien@bk.ru>
2023-10-25 20:05:22 +0300

    дополняет исправления

--- js/photosArray.js
+++ js/photosArray.js
@@ -1,22 +1,19 @@
 import { getRandomInteger, getRandomArrayElement, createIdGenerator } from './utils.js';
 import {
+  data,
   PICTURE_COUNT,
   AVATAR_COUNT,
   LIKE_MIN_COUNT,
   LIKE_MAX_COUNT,
-  COMMENT_COUNT,
-  COMMENT_LINES,
-  DESCRIPTION_ARRAY,
-  NAMES_ARRAY
-} from './data.js';
+  COMMENT_COUNT } from './data.js';
 
 const generateCommentId = createIdGenerator();
 
 const createComment = () => ({
   id: generateCommentId(),
   avatar: `img/avatar-${getRandomInteger(1, AVATAR_COUNT)}.svg`,
-  message: getRandomArrayElement(COMMENT_LINES),
-  name: getRandomArrayElement(NAMES_ARRAY),
+  message: getRandomArrayElement(data().COMMENT_LINES),
+  name: getRandomArrayElement(data().NAMES_ARRAY),
 });
 
 const generatePhotoId = createIdGenerator();
@@ -24,7 +21,7 @@ const generatePhotoId = createIdGenerator();
 const createPicture = (index) => ({
   id: generatePhotoId(),
   url: `photos/${index}.jpg`,
-  description: getRandomArrayElement(DESCRIPTION_ARRAY),
+  description: getRandomArrayElement(data().DESCRIPTION_ARRAY),
   likes: getRandomInteger(LIKE_MIN_COUNT, LIKE_MAX_COUNT),
   comments: Array.from(
     { length: getRandomInteger(0, COMMENT_COUNT) },
